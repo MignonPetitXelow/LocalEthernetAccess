6e93fabaa14c5d832bec7e8797e81ed5cfb08258
Xelow <XELOXLEGRANDE@gmail.com>
2021-10-31 16:27:55 +0000

    Import

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.IO;
+
+namespace LocalIpFounder
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String AppPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            String LogPath = AppPath+@"\latest.log";
+            String BuildVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("LOCAL IP FOUNDER.   ");
+            Console.WriteLine("Please write \"help\". ");
+            while (true)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(Environment.UserName+": ");
+                String cmd = Console.ReadLine();
+
+                if (cmd == "help")
+                {
+                    Console.WriteLine("|- help");
+                    Console.WriteLine("|- search");
+                    Console.WriteLine("|- version");
+                    Console.WriteLine(" ");
+                }
+                if (cmd == "search")
+                {
+                    foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Name: " + netInterface.Name);
+                        Console.WriteLine("Description: " + netInterface.Description);
+                        Console.WriteLine("Addresses: ");
+                        IPInterfaceProperties ipProps = netInterface.GetIPProperties();
+                        foreach (UnicastIPAddressInformation addr in ipProps.UnicastAddresses)
+                        {
+                            Console.WriteLine(" " + addr.Address.ToString());
+                        }
+                        Console.WriteLine("");
+                    }
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                if (cmd == "version")
+                {
+                    Console.WriteLine("Build Version: "+BuildVersion);
+                }
+            }
+        }
+    }
+}
